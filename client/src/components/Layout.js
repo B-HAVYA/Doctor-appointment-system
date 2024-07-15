fe9f25f71f697ce9a1d166aa7a6b20146c747c59
Bhavya Upadhyay <bhavyaupadhyay3@gmail.com>
2024-07-15 19:41:29 +0530

    Initial commit

---
+++ client/src/components/Layout.js
@@ -0,0 +1,98 @@
+import React from "react";
+import "../styles/LayoutStyles.css";
+import { adminMenu, userMenu } from "./../Data/data";
+
+import { Link, useLocation, useNavigate } from "react-router-dom";
+import { useSelector } from "react-redux";
+import { Badge, message } from "antd";
+const Layout = ({ children }) => {
+  const { user } = useSelector((state) => state.user);
+  const location = useLocation();
+  const navigate = useNavigate();
+  // logout funtion
+  const handleLogout = () => {
+    localStorage.clear();
+    message.success("Logout Successfully");
+    navigate("/login");
+  };
+
+  // =========== doctor menu ===============
+  const doctorMenu = [
+    {
+      name: "Home",
+      path: "/",
+      icon: "fa-solid fa-house",
+    },
+    {
+      name: "Appointments",
+      path: "/doctor-appointments",
+      icon: "fa-solid fa-list",
+    },
+
+    {
+      name: "Profile",
+      path: `/doctor/profile/${user?._id}`,
+      icon: "fa-solid fa-user",
+    },
+  ];
+  // =========== doctor menu ===============
+
+  // redering menu list
+  const SidebarMenu = user?.isAdmin
+    ? adminMenu
+    : user?.isDoctor
+    ? doctorMenu
+    : userMenu;
+  return (
+    <>
+      <div className="main">
+        <div className="layout">
+          <div className="sidebar">
+            <div className="logo">
+            
+           <span><center><img color="white" src="https://flaticons.net/icon.php?slug_category=people&slug_icon=doctor" width="200" align=""/></center>
+             <h6 className="text-light">DOC APPOINTMENT SYSTEM</h6> </span>
+              <hr />
+            </div>
+            <div className="menu">
+              {SidebarMenu.map((menu) => {
+                const isActive = location.pathname === menu.path;
+                return (
+                  <>
+                    <div className={`menu-item ${isActive && "active"}`}>
+                      <i className={menu.icon}></i>
+                      <Link to={menu.path}>{menu.name}</Link>
+                    </div>
+                  </>
+                );
+              })}
+              <div className={`menu-item `} onClick={handleLogout}>
+                <i className="fa-solid fa-right-from-bracket"></i>
+                <Link to="/login">Logout</Link>
+              </div>
+            </div>
+          </div>
+          <div className="content">
+          <div className="header" style={{ backgroundColor: '#E0F4FF' }}>
+              <div className="header-content" style={{ cursor: "pointer" }}>
+                <Badge
+                  count={user && user.notifcation.length}
+                  onClick={() => {
+                    navigate("/notification");
+                  }}
+                >
+                  <i class="fa-solid fa-bell"></i>
+                </Badge>
+
+                <Link to="/profile">{user?.name}</Link>
+              </div>
+            </div>
+            <div className="body" style={{ backgroundColor: '#E0F4FF' }}>{children}</div>
+          </div>
+        </div>
+      </div>
+    </>
+  );
+};
+
+export default Layout;
